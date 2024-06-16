95916c532b67407bf2d0be9d4e4825d9977e2726
LEONARDO SCHULER <leonardo_schuler55@hotmail.com>
2024-06-16 09:37:26 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,52 @@
+let quantidadePista = 100;
+let quantidadeSuperior = 200;
+let quantidadeInferior = 400;
+
+function comprar() {
+    //recuperar os dados do tipo e quantidade
+    let tipoIngresso = document.getElementById('tipo-ingresso').value;
+    let quantidade = document.getElementById('qtd').value;
+    
+    //diminuir do valor total dos ingressos a quantidade comprada 
+    if (tipoIngresso == 'superior'){
+        if (quantidadeSuperior == 0) {
+            alert ('Os ingressos deste tipo estão esgotados!');
+        } else{
+            if (quantidade > quantidadeSuperior) {
+                alert ('Altere a quantidade escolhida!');
+            } else {
+                quantidadeSuperior = quantidadeSuperior - quantidade;
+                let escritoQuantidadeSuperior = document.getElementById('qtd-superior');
+                escritoQuantidadeSuperior.textContent = `${quantidadeSuperior}`;
+            }
+        }
+    } 
+
+    if (tipoIngresso == 'inferior'){
+        if (quantidadeInferior == 0) {
+            alert ('Os ingressos deste tipo estão esgotados!');
+        } else {
+            if (quantidade > quantidadeInferior) {
+                alert ('Altere a quantidade escolhida!');
+            } else {
+                quantidadeInferior = quantidadeInferior - quantidade;
+                let escritoQuantidadeInferior = document.getElementById('qtd-inferior');
+                escritoQuantidadeInferior.textContent = `${quantidadeInferior}`;
+            }
+        }
+    }
+
+    if (tipoIngresso == 'pista') {
+        if (quantidadePista == 0) {
+            alert ('Os ingressos deste tipo estão esgotados!');
+        } else {
+            if (quantidade > quantidadePista){
+                alert ('Altere a quantidade escolhida!');
+            } else {
+                quantidadePista = quantidadePista - quantidade;
+                let escritoQuantidadePista = document.getElementById('qtd-pista');
+                escritoQuantidadePista.textContent = `${quantidadePista}`;
+            }
+        }
+    }
+}   
